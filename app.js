0a64a5ea1dd77737876eda5526925f8b9fbb8bf1
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-11-04 14:49:38 +0500

    drap and drop function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const gameBoard = document.querySelector('#gameboard')
 const playerDisplay = document.querySelector('#player')
 const infoDisplay = document.querySelector('#info-display')
 const width = 8
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
 
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -25,7 +27,6 @@ function createBoard() {
         square.setAttribute('square-id', i)
         // square.classList.add('beige')
         const row = Math.floor(i / width)
-        console.log(row)
         if(row % 2 === 0 ) {
             square.classList.add(i % 2 === 0 ? 'beige' : 'brown')
             
@@ -45,3 +46,69 @@ function createBoard() {
 }
 
 createBoard()
+
+
+const allSquares = document.querySelectorAll('#gameboard .square')
+
+// console.log(all)
+
+allSquares.forEach((square)=> {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId;
+let draggedElement
+
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+}
+
+function dragOver(e) {
+    e.preventDefault()
+    // console.log(e.target)
+}
+function dragDrop(e) {
+    // e.preventDefault()
+    e.stopPropagation()
+    console.log(e.target)
+    const taken = e.target.classList.contains('piece')
+
+    // console.log(e.target.parentNode.append(draggedElement))
+    // e.target.parentNode.append(draggedElement)
+    // e.target.remove()
+    // e.target.append(draggedElement)
+    changePlayer()
+}
+
+function changePlayer() {
+    if (playerGo === 'black') {
+        reverseId()
+        playerGo = 'white'
+        playerDisplay.textContent = 'white'
+    } else {
+        revertId()
+        playerGo = 'black'
+        
+        playerDisplay.textContent = 'black'
+        
+    }
+}
+
+
+function reverseId () {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i)=> {
+            square.setAttribute('square-id', (width * width - 1) - i)
+            
+        })
+}
+function revertId () {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i)=> {
+            square.setAttribute('square-id', i)
+
+        })
+}
